21ce9ea5fbab0025651a13ffbe1b1813de59807a
Taylor Lewis <TaylorJordanLewis@gmail.com>
2017-07-23 20:46:30 -0700

    Write CPU initialization

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -1,5 +1,17 @@
 #include "CPU.hpp"
 
 CPU::CPU() {
+    r_a = 0;
+    r_x = 0;
+    r_y = 0;
 
+    r_sp = 0xFD;
+
+    r_p = 0b00110100;
+
+    // TODO: What is the PC set to?
+
+    for (uint8_t &elem : memory) {
+        elem = 0;
+    }
 }
