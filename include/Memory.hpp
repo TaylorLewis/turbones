a36139f645b397d3a47c8638ee19259e7f66caab
Taylor Lewis <TaylorJordanLewis@gmail.com>
2017-07-29 12:17:02 -0700

    Start Memory class

---
+++ include/Memory.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdint.h>
+#include <array>
+
+// Memory.
+//   $0000 -$07FF
+//    2 KB internal RAM.
+//     $0000-$00FF
+//      Zero page. Addressable with a single byte.
+//   $0800-$1FFF
+//    Mirrors (3) of RAM.
+//   $2000-$2007
+//    PPU registers.
+//   $2008-$3FFF
+//    Mirrors (1023) of PPU registers.
+//   $4000-$4017
+//    APU and I/O registers
+//   $4018-$401F
+//    APU and I/O functionality that is normally disabled.
+//   $4020-$FFFF
+//    Cartridge space: PRG ROM, PRG RAM, and mapper registers. 
+class Memory {
+public:
+    Memory();
+
+    uint8_t read(const uint16_t address);
+    void write(const uint16_t address, const uint8_t value);
+
+private:
+    std::array<uint8_t, 0x800> ram;
+};
