1f81b9ed4970eb9db1943e827bbf6c0379b18645
Taylor Lewis <TaylorJordanLewis@gmail.com>
2017-08-03 00:23:05 -0700

    Add some registers and functions to PPU header

--- include/PPU.hpp
+++ include/PPU.hpp
@@ -7,9 +7,24 @@
 // Generates video.
 class PPU {
 public:
+    void powerOn();
 
+    uint8_t readRegister(const uint16_t& address);
+    void writeRegister(const uint16_t& address, const uint8_t& value);
 
 private:
+    // Registers
+    uint8_t controller,  // $2000: PPUCTRL
+            mask,        // $2001: PPUMASK
+            status,      // $2002: PPUSTATUS
+            oam_address, // $2003: OAMADDR
+            oam_data,    // $2004: OAMDATA
+            scroll,      // $2005: PPUSCROLL
+            address,     // $2006: PPUADDR
+            data,        // $2007: PPUDATA
+            oam_dma;     // $4014: OAMDMA
+
+
     // Object Attribute Memory
     std::array<uint8_t, 0xFF> OAM;
 
