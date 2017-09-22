662368966e8e654c8f10325ac0272f15dab78635
Taylor Lewis <TaylorJordanLewis@gmail.com>
2017-09-21 23:51:41 -0700

    Start writing preliminary PPU structure

--- include/PPU.hpp
+++ include/PPU.hpp
@@ -8,11 +8,15 @@
 class PPU {
 public:
     void powerOn();
+    void step();
 
     uint8_t readRegister(const uint16_t& address);
     void writeRegister(const uint16_t& address, const uint8_t& value);
 
 private:
+    static constexpr int WIDTH  = 256,
+                         HEIGHT = 240;
+
     // Registers
     uint8_t controller,  // $2000: PPUCTRL
             mask,        // $2001: PPUMASK
@@ -24,8 +28,15 @@ private:
             data,        // $2007: PPUDATA
             oam_dma;     // $4014: OAMDMA
 
+    uint16_t vram_address;      // Current vram address.   (15 bit)
+    uint16_t vram_address_temp; // Temporary vram address. (15 bit)
+    uint8_t  x;                 // Fine x scroll            (3 bit)
+    bool     w;                 // Write toggle             (1 bit)
+    bool     f;                 // Even/odd frame flag      (1 bit)
 
-    // Object Attribute Memory
-    std::array<uint8_t, 0xFF> OAM;
 
+    // Object Attribute Memory, aka Sprite RAM.
+    std::array<uint8_t, 0xFF> oam;
+
+    std::array<uint8_t, 0x3FFF> vram;
 };
--- src/PPU.cpp
+++ src/PPU.cpp
@@ -1,2 +1,62 @@
 #include "PPU.hpp"
 
+void PPU::powerOn() {
+
+}
+
+void PPU::step() {
+
+}
+
+uint8_t PPU::readRegister(const uint16_t & address) {
+    switch (address) {
+        case 0x2002:
+            //return readStatus();
+            break;
+        case 0x2004:
+            //return readOAMData();
+            break;
+        case 0x2007:
+            //return readData();
+            break;
+        default:
+            // TODO: ERROR
+            break;
+    }
+
+
+    return 0; // d
+}
+
+void PPU::writeRegister(const uint16_t & address, const uint8_t & value) {
+    switch (address) {
+        case 0x2000:
+            //writeControl(value);
+            break;
+        case 0x2001:
+            //writeMask(value);
+            break;
+        case 0x2003:
+            //writeOAMAddress(value);
+            break;
+        case 0x2004:
+            //writeOAMData(value);
+            break;
+        case 0x2005:
+            //writeScroll(value);
+            break;
+        case 0x2006:
+            //writeAddress(value);
+            break;
+        case 0x2007:
+            //writeData(value);
+            break;
+        case 0x4014:
+            //writeDMA(value);
+            break;
+        default:
+            // TODO: ERROR
+            break;
+    }
+}
+
